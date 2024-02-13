a44be281ad45705a8f06e73c93864d334491925a
rakawidjaja169 <raka.widjaja169@gmail.com>
2024-02-13 22:20:00 +0700

    Carousel Product

--- resources/js/Pages/Product/Show.vue
+++ resources/js/Pages/Product/Show.vue
@@ -4,6 +4,10 @@ import { Inertia } from "@inertiajs/inertia";
 import { ref } from "vue";
 import {usePage} from '@inertiajs/inertia-vue3'
 
+import { Carousel, Slide } from 'vue3-carousel'
+
+import 'vue3-carousel/dist/carousel.css'
+
 const props = defineProps({
     product: Object,
 });
@@ -32,6 +36,12 @@ const productImage = () => {
 
     return '/images/' + preview.url;
 }
+
+// Expose values, functions, or computed properties as needed:
+const currentSlide = ref(0);
+const slideTo = (val) => {
+  currentSlide.value = val;
+};
 </script>
 
 <template>
@@ -88,5 +98,25 @@ const productImage = () => {
                 </div>
             </div>
         </div>
+
+        <Carousel id="gallery" :items-to-show="1" :wrap-around="false" v-model="currentSlide">
+            <Slide v-for="slide in 10" :key="slide">
+                <!-- <div class="carousel__item">{{ slide }}</div> -->
+                <img :src="productImage()" alt="product_image" class="carousel__item">
+            </Slide>
+        </Carousel>
+
+        <Carousel
+            id="thumbnails"
+            :items-to-show="4"
+            :wrap-around="true"
+            v-model="currentSlide"
+            ref="carousel"
+        >
+            <Slide v-for="slide in 10" :key="slide">
+                <!-- <div class="carousel__item" @click="slideTo(slide - 1)">{{ slide }}</div> -->
+                <img :src="productImage()" alt="product_image" class="carousel__item" @click="slideTo(slide - 1)">
+            </Slide>
+        </Carousel>
     </ShopLayout>
 </template>
